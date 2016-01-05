342a3c182adbc1497a7c860f74071063d7879f5f
necropotame <necropotame@gmail.com>
2016-01-05 11:37:25 +0100

    First draft for multisnapshot

--- src/modapi/server/entity.cpp
+++ src/modapi/server/entity.cpp
@@ -6,19 +6,21 @@ CModAPI_EntitySnapshot07::CModAPI_EntitySnapshot07(CGameWorld *pGameWorld, int O
 	CEntity(pGameWorld, ObjType, Pos, ProximityRadius)
 {
 	m_NbIdPerSnapshot = NbIdPerSnapshot;
-	m_IDList.set_size(m_NbIdPerSnapshot);
+	m_IDList.set_size(2*m_NbIdPerSnapshot);
 	
-	for(int i=0; i<m_IDList.size(); i++)
+	for(int i=0; i<m_NbIdPerSnapshot; i++)
 	{
-		m_IDList[i] = Server()->SnapNewID();
+		m_IDList[i] = Server()->SnapNewID07();
+		m_IDList[NbIdPerSnapshot+i] = Server()->SnapNewID07ModAPI();
 	}
 }
 
 CModAPI_EntitySnapshot07::~CModAPI_EntitySnapshot07()
 {
-	for(int i=0; i<m_IDList.size(); i++)
+	for(int i=0; i<m_NbIdPerSnapshot; i++)
 	{
-		Server()->SnapFreeID(m_IDList[i]);
+		Server()->SnapFreeID07(m_IDList[i]);
+		Server()->SnapFreeID07ModAPI(m_IDList[m_NbIdPerSnapshot+i]);
 	}
 }
 
@@ -34,7 +36,7 @@ void CModAPI_EntitySnapshot07::Snap07(int SnappingClient)
 
 void CModAPI_EntitySnapshot07::Snap07ModAPI(int SnappingClient)
 {
-	Snap(SnappingClient, 0);
+	Snap(SnappingClient, m_NbIdPerSnapshot);
 }
 
 int CModAPI_EntitySnapshot07::GetID(int ID)
